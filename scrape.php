8ba2f6e15a9b02e27117a30f67933a735481e86e
Chirayu Asati <chirayuasati19@gmail.com>
2017-04-01 05:14:57 +0530

    Solved issue of images

--- scrape.php
+++ scrape.php
@@ -13,7 +13,7 @@
 				width: 100%;
 				height: 100%;
 				z-index: 9999;
-				background: url('page-loader.gif') 50% 50% no-repeat rgb(249,249,249);
+				background: url('images/page-loader.gif') 50% 50% no-repeat rgb(249,249,249);
 				opacity: .8;
 			}
 
